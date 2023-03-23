d2cc9ce9abbe44e0dbe46e0988e5e26d8e7ee048
seigo07 <seven_8s@icloud.com>
2023-03-23 05:08:07 +0000

    add userDetail

---
+++ spa/pages/userDetail.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-app>
+    <v-form>
+      <v-container>
+        <v-card>
+          <v-card-title>
+            User Detail
+            <v-spacer/>
+          </v-card-title>
+        </v-card>
+        <v-row>
+          <v-col>
+            <v-select v-model="role" :items="roles" label="Roles"></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field label="UserName" v-model="userName"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn color="primary"
+                   :disabled="!this.userName || !this.role" @click="updateUser()">
+              Update
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn color="primary" @click="deleteUser()">
+              Delete
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-app>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  layout: "index",
+  data() {
+    return {
+      role: '',
+      roles: ["admin", "user"],
+      userName: '',
+      password: '',
+      error: "",
+    }
+  },
+  mounted() {
+    this.userName = this.$route.query.userName
+    this.role = this.$route.query.role
+    // axios.get('/user', {
+    //   "userName": this.userName,
+    // }).then((response) => {
+    //   this.userName = response.data.userName
+    //   this.password = response.data.password
+    //   this.role = response.data.role
+    //   console.log("response.data: ", response.data)
+    //   // this.error = response.data.length == 0 ? "No Bus Route found" : "";
+    // }).catch((error) => {
+    //   console.log('There is error:' + error.response)
+    // })
+  },
+  methods: {
+    async updateUser() {
+      await axios.put('/user', {
+        "userName": this.userName,
+        "role": this.role == "admin" ? 1 : 2,
+      }).then((response) => {
+        console.log("response.data: ", response.data)
+      }).catch((error) => {
+        console.log('There is error:' + error.response)
+      })
+    },
+    async deleteUser() {
+      await axios.delete('/user', {
+        "userName": this.userName,
+      }).then((response) => {
+        console.log("response.data: ", response.data)
+      }).catch((error) => {
+        console.log('There is error:' + error.response)
+      })
+    }
+  },
+}
+
+</script>
--- spa/pages/userList.vue
+++ spa/pages/userList.vue
@@ -3,7 +3,7 @@
     <v-card>
       <v-card-title>
         User List
-        <v-spacer />
+        <v-spacer/>
       </v-card-title>
     </v-card>
     <v-row>
@@ -65,17 +65,12 @@ export default {
   },
   methods: {
     handleClick(row) {
-      console.log(row.userName)
-    },
-    async getUser() {
-      await axios.get('/user', {
-        "userName": this.userName,
-      }).then((response) => {
-        console.log("response.data: ", response.data)
-        // this.error = response.data.length == 0 ? "No Bus Route found" : "";
-      }).catch((error) => {
-        console.log('There is error:' + error.response)
-      })
+      this.$router.push({
+        path: '/userDetail', query: {
+          userName: row.userName,
+          role: row.role
+        }
+      });
     },
   },
 }
