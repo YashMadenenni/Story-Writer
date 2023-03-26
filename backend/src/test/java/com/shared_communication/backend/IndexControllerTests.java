8231b0e7b21ceb92373cf76581447b9cec9cb63a
Yashwanth Kumar <ykms1@st-andrews.ac.uk>
2023-03-26 23:20:53 +0100

    Integrating test cases with Index controller

--- backend/src/test/java/com/shared_communication/backend/IndexControllerTests.java
+++ backend/src/test/java/com/shared_communication/backend/IndexControllerTests.java
@@ -5,42 +5,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest (classes = IndexControllerTests.class)
+@SpringBootTest
 @AutoConfigureMockMvc
-class IndexControllerTests { 
+public class IndexControllerTests { 
 
 	@Autowired
 	private MockMvc mockMvc;
 
+    Model model = new Model("src/main/resources/static/users.json", "src/main/resources/static/admin.json");
+	
+
 	//Test1
 	@Test 
 	public void testUserRegisteration() throws Exception{
+				mockMvc.perform(MockMvcRequestBuilders.post("/user").content("{\"userEmail\":\"testIndex@gmail.com\", \"userName\":\"Test\", \"password\":\"testIndex\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+
+	}
+
+	
+
+	//Test4
+	@Test
+	public void testUserlogin() throws Exception{
 		
-		mockMvc.perform(MockMvcRequestBuilders.post("/user").content("{userEmail:\"test@gmail.com\", userName:\"Test\", password:\"test\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders.post("/userLogin").content("{\"userEmail\":\"testIndex@gmail.com\", \"userName\":\"Test\", \"password\":\"testIndex\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+		//p
+	//Test7
+	@Test
+	public void testDeleteUserSystemLevel () throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.delete("/user?userEmail=test@gmail.com")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	//Test2
 	@Test 
 	public void testAdminRegisteration() throws Exception{
 		
-		mockMvc.perform(MockMvcRequestBuilders.post("/admin").content("{adminEmail:\"test@gmail.com\", userName:\"Test\", password:\"test\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders.post("/admin").content("{\"userEmail\":\"testIndexAdmin@gmail.com\", \"userName\":\"Test\", \"password\":\"testIndexAdmin\"}")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	//Test3
 	@Test 
 	public void testAdminlogin() throws Exception{
-		
-		mockMvc.perform(MockMvcRequestBuilders.post("/adminLogin").content("{adminEmail:\"test@gmail.com\", password:\"test\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders.post("/admin").content("{\"userEmail\":\"testLoginAdmin@gmail.com\", \"userName\":\"Test\", \"password\":\"testLoginAdmin\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders.post("/adminLogin").content("{\"userEmail\":\"testLoginAdmin@gmail.com\", \"userName\":\"Test\", \"password\":\"testLoginAdmin\"}")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
-	//Test4
 	@Test
-	public void testUserlogin() throws Exception{
-		
-		mockMvc.perform(MockMvcRequestBuilders.post("/userLogin").content("{userEmail:\"test@gmail.com\", password:\"test\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+	public void testDeleteUserAdminSystemLevel () throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.delete("/user?userEmail=admin@gmail.com")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	//p
@@ -58,12 +75,7 @@ class IndexControllerTests {
 		mockMvc.perform(MockMvcRequestBuilders.put("/page").content(jsonBody)).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
-	//p
-	//Test7
-	@Test
-	public void testDeleteUserSystemLevel () throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.delete("/user?userName=test@gmail.com")).andExpect(MockMvcResultMatchers.status().isOk());
-	}
+
 	
 	//p
 	//Test8
@@ -93,12 +105,12 @@ class IndexControllerTests {
 		mockMvc.perform(MockMvcRequestBuilders.delete("/access?pageName=Communication&userName=test2@gmail.com")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
-	@Test
-	//p
-	//Test12
-	public void testEditProfile() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.put("/user").content("{userName:\"new Email\"}")).andExpect(MockMvcResultMatchers.status().isOk());
-	}
+	// @Test
+	// //p
+	// //Test12
+	// public void testEditProfile() throws Exception {
+	// 	mockMvc.perform(MockMvcRequestBuilders.put("/user").content("{userName:\"new Email\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+	// }
 
 	@Test
 	//p
@@ -106,7 +118,12 @@ class IndexControllerTests {
 	public void testDeletePage() throws Exception{
 		mockMvc.perform(MockMvcRequestBuilders.delete("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
-
+//Test15
+	//Test5
+	@Test
+	public void testMultiplePagewithSameName() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.post("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().isOk()).andDo(result -> mockMvc.perform(MockMvcRequestBuilders.post("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().isUnauthorized()));
+	}
 	@Test
 	//Test14
 	public void testUsersList() throws Exception{
@@ -124,22 +141,17 @@ class IndexControllerTests {
 	@Test
 	public void testAdminInvalidlogin() throws Exception{
 		
-		mockMvc.perform(MockMvcRequestBuilders.post("/adminLogin").content("{adminEmail:\"test@gmail.com\", password:\"tet\"}")).andExpect(MockMvcResultMatchers.status().isUnauthorized());
+		mockMvc.perform(MockMvcRequestBuilders.post("/adminLogin").content("{\"userEmail\":\"testIndexAdminInvalid@gmail.com\", \"userName\":\"Test\", \"password\":\"testIndexAdminInvalid\"}")).andExpect(MockMvcResultMatchers.status().isUnauthorized());
 	}
 
-	//Test15
-	//Test5
-	@Test
-	public void testMultiplePagewithSameName() throws Exception{
-		mockMvc.perform(MockMvcRequestBuilders.post("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().isOk()).andDo(result -> mockMvc.perform(MockMvcRequestBuilders.post("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().isUnauthorized()));
-	}
+	
 
 	//Test16
 	//Test4
 	@Test
 	public void testUserInvalidlogin() throws Exception{
 		
-		mockMvc.perform(MockMvcRequestBuilders.post("/userLogin").content("{userEmail:\"test@gmail.com\", password:\"test\"}")).andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders.post("/userLogin").content("{\"userEmail\":\"test35@gmail.com\", \"userName\":\"Test35\", \"password\":\"test35\"}")).andExpect(MockMvcResultMatchers.status().is4xxClientError());
 	}
 
 	//p
@@ -147,14 +159,14 @@ class IndexControllerTests {
 	//Test13
 	@Test
 	public void testDeletePageInvalidName() throws Exception{
-		mockMvc.perform(MockMvcRequestBuilders.delete("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().isNotFound());
+		mockMvc.perform(MockMvcRequestBuilders.delete("/page?pageName=Communication")).andExpect(MockMvcResultMatchers.status().is4xxClientError());
 	}
 
 	//Test18
 	//Test15
 	@Test
 	public void testGetUserInvalidName() throws Exception{
-		mockMvc.perform(MockMvcRequestBuilders.get("/user?userName=Test@gmail.com")).andExpect(MockMvcResultMatchers.status().isNotFound());
+		mockMvc.perform(MockMvcRequestBuilders.get("/user?userName=Test@gmail.com")).andExpect(MockMvcResultMatchers.status().is4xxClientError());
 	}
 
 	//p
@@ -162,7 +174,7 @@ class IndexControllerTests {
 	//Test7
 	@Test
 	public void testDeleteUserSystemLevelInvalidName () throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.delete("/user?userName=tes@gmail.com")).andExpect(MockMvcResultMatchers.status().isNotFound());
+		mockMvc.perform(MockMvcRequestBuilders.delete("/user?userName=tes@gmail.com")).andExpect(MockMvcResultMatchers.status().is4xxClientError());
 	}
 	
 	//p
